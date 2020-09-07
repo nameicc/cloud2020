be286af02adbbb7270f6fd923d026bf06c3b190d
shichuanfeng <shichuanfeng@topscomm.com>
2020-09-07 19:27:55 +0800

    项目初始化

---
+++ cloud-provider-payment8001/src/main/java/com/tingyu/cloud/App.java
@@ -0,0 +1,16 @@
+package com.tingyu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: tingyu
+ * @Date: 2020-09-04 17:26
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
