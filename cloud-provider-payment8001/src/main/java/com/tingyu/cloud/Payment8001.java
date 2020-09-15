a00e631b6940ede144770b83aa6daf5a73450b32
shichuanfeng <shichuanfeng@topscomm.com>
2020-09-15 19:46:42 +0800

    payment集成eureka

--- cloud-provider-payment8001/src/main/java/com/tingyu/cloud/Payment8001.java
+++ cloud-provider-payment8001/src/main/java/com/tingyu/cloud/Payment8001.java
@@ -2,6 +2,7 @@ package com.tingyu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description:
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2020-09-04 17:26
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001 {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001.class, args);
