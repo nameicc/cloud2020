d9e5cd275fef14b3cf50bf9b98967ae239dcab57
shichuanfeng <shichuanfeng@topscomm.com>
2020-09-16 18:40:47 +0800

    order集成eureka

--- cloud-consumer-order80/src/main/java/com/tingyu/cloud/Order80.java
+++ cloud-consumer-order80/src/main/java/com/tingyu/cloud/Order80.java
@@ -2,6 +2,7 @@ package com.tingyu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2020/9/10 16:38
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class Order80 {
 
     public static void main(String[] args) {
