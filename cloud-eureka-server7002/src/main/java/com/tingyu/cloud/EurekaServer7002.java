fba33e2cbe06bf550b561d36a105d70b2a8567a8
shichuanfeng <shichuanfeng@topscomm.com>
2020-09-16 19:37:01 +0800

    eureka集群

---
+++ cloud-eureka-server7002/src/main/java/com/tingyu/cloud/EurekaServer7002.java
@@ -0,0 +1,20 @@
+package com.tingyu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description
+ * @Author tingyu
+ * @Date 2020/9/16 19:20
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002.class, args);
+    }
+
+}
