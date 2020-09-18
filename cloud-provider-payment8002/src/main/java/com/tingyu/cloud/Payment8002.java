b992e6ca3c09b4a0c8e64272c8ae4d0d7c42356e
shichuanfeng <shichuanfeng@topscomm.com>
2020-09-18 19:21:26 +0800

    payment服务集群，负载均衡调用

---
+++ cloud-provider-payment8002/src/main/java/com/tingyu/cloud/Payment8002.java
@@ -0,0 +1,18 @@
+package com.tingyu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author: tingyu
+ * @Date: 2020-09-04 17:26
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class Payment8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8002.class, args);
+    }
+}
