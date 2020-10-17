d5256cb3e6b7dfa0b8f2caf1ce4c35beec94262d
shichuanfeng <shichuanfeng@topscomm.com>
2020-10-17 17:12:40 +0800

    restTemplate调用getForEntity

--- cloud-consumer-order80/src/main/java/com/tingyu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/tingyu/cloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.tingyu.cloud.controller;
 import com.tingyu.cloud.entity.CommonResult;
 import com.tingyu.cloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,16 @@ public class OrderController {
     @GetMapping("consumer/payment/get/{id}")
     public CommonResult getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("consumer/payment/getForEntity/{id}")
+    public CommonResult getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult(444, "请求错误");
+        }
     }
 
 
