0c06bdfd52022325e6242de0e6675c66873f3063
shichuanfeng <shichuanfeng@topscomm.com>
2020-10-23 16:58:23 +0800

    Payment服务端  服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/tingyu/cloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/tingyu/cloud/service/PaymentService.java
@@ -1,6 +1,9 @@
 package com.tingyu.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @Description
@@ -14,13 +17,21 @@ public class PaymentService {
         return "线程：" + Thread.currentThread().getName() + ", id：" + id + ", status is ok";
     }
 
+    @HystrixCommand(fallbackMethod = "timeOutFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value="5000")
+    })
     public String payment_timeout(long id){
+        int a = 10 / 0;
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程：" + Thread.currentThread().getName() + ", id：" + id + ", status is timeout";
+    }
+
+    public String timeOutFallback(@PathVariable("id") long id){
+        return "payment service timeout fallback";
     }
 
 }
