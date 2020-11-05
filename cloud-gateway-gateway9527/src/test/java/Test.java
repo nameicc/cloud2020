54278e8488a67d730cbf78210ccb3b048321fa23
shichuanfeng <shichuanfeng@topscomm.com>
2020-11-05 19:24:31 +0800

    Gateway的常用断言

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Description
+ * @Author tingyu
+ * @Date 2020/11/5 19:17
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+
+}
