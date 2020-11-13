6cb8716b1f2d3926c6a490e6b41bd998d96607a2
shichuanfeng <shichuanfeng@topscomm.com>
2020-11-13 19:56:21 +0800

    同分组消费者的重复消费问题

---
+++ cloud-stream-rabbit-consumer8803/src/main/java/com/tingyu/cloud/StreamRabbitConsumer8803.java
@@ -0,0 +1,18 @@
+package com.tingyu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author tingyu
+ * @Date 2020/11/13 19:50
+ **/
+@SpringBootApplication
+public class StreamRabbitConsumer8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbitConsumer8803.class, args);
+    }
+
+}
