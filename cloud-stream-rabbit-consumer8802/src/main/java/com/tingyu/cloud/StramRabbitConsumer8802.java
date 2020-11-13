cf44fcd7660297d2f491bf408b20ee302a399f25
shichuanfeng <shichuanfeng@topscomm.com>
2020-11-13 19:21:38 +0800

    Stream搭建RabbitMQ队列的消费者

---
+++ cloud-stream-rabbit-consumer8802/src/main/java/com/tingyu/cloud/StramRabbitConsumer8802.java
@@ -0,0 +1,18 @@
+package com.tingyu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author tingyu
+ * @Date 2020/11/13 19:13
+ **/
+@SpringBootApplication
+public class StramRabbitConsumer8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StramRabbitConsumer8802.class, args);
+    }
+
+}
---
+++ cloud-stream-rabbit-consumer8802/src/main/java/com/tingyu/cloud/service/MessageConsumerService.java
@@ -0,0 +1,28 @@
+package com.tingyu.cloud.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description
+ * @Author tingyu
+ * @Date 2020/11/13 19:14
+ **/
+@Component
+@EnableBinding(Sink.class)
+public class MessageConsumerService {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void getMessage(Message<String> message){
+        System.out.println("消费者" + serverPort + " receive : " + message.getPayload());
+    }
+
+
+}
