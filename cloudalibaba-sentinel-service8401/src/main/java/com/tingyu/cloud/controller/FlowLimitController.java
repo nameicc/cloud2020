fc74c1024bffa21cefbc5eff2bb1784f98544094
shichuanfeng <shichuanfeng@topscomm.com>
2020-11-27 18:15:29 +0800

    Sentinel流控测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/tingyu/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/tingyu/cloud/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.tingyu.cloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2020/11/26 21:01
  **/
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA(){
+        /*try {
+            Thread.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         return "testA.........";
     }
 
     @GetMapping("/testB")
     public String testB(){
+        log.info(Thread.currentThread().getName() + "....testB");
         return "testB.........";
     }
 
