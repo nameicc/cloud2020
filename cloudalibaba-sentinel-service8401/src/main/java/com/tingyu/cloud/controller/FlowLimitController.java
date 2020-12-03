2fd35904dd15fd28ac7ff60397049756cd46c8a7
shichuanfeng <shichuanfeng@topscomm.com>
2020-12-03 14:08:00 +0800

    Sentinel热点规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/tingyu/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/tingyu/cloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.tingyu.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -50,6 +53,16 @@ public class FlowLimitController {
     public String testE(){
         int a = 10 / 0;
         return "testE.........";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "testHotKeyHandler")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1, @RequestParam(value = "p2", required = false) String p2){
+        return "hot key test....";
+    }
+
+    public String testHotKeyHandler(String p1, String p2, BlockException exception){
+        return "this is hot key handler....";
     }
 
 }
