32caedc08543d3c290446719a513cf3ee1946d0b
shichuanfeng <shichuanfeng@topscomm.com>
2020-12-03 16:58:34 +0800

    Sentinel Fallback管运行异常

--- cloudalibaba-consumer-Order84/src/main/java/com/tingyu/cloud/controller/OrderController.java
+++ cloudalibaba-consumer-Order84/src/main/java/com/tingyu/cloud/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.tingyu.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.tingyu.cloud.entity.CommonResult;
+import com.tingyu.cloud.entity.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +24,22 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/{id}")
-    public CommonResult getPayment(@PathVariable("id") String id){
+    @SentinelResource(value = "order", fallback = "payment_fallback")
+    public CommonResult getPayment(@PathVariable("id") long id){
         CommonResult result = restTemplate.getForObject(SERVICE_URL + "/payment/" + id, CommonResult.class);
+
+        if(id == 4){
+            throw new IllegalArgumentException("非法的id！");
+        }else if(result.getData() == null){
+            throw new NullPointerException("空指针异常！");
+        }
+
         return result;
+    }
+
+    public CommonResult payment_fallback(long id, Throwable e){
+        Payment payment = new Payment(id, "");
+        return new CommonResult(444, "fallback handler, exception is " + e.getMessage(), payment);
     }
 
 }
